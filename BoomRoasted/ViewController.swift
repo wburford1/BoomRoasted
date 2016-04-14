8abf721fd5d3de59acc7520d338ee98b4e7065d5
wburford1 <william_burford@wills-air.internal.hws.org>
2016-04-14 12:37:15 -0700

    boom!

--- BoomRoasted/ViewController.swift
+++ BoomRoasted/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var currentSound = String()
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let height = self.view.frame.size.height
+        let width = self.view.frame.size.width
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.init(white: 0.5, alpha: 1)
+        
+        let button = UIButton.init(frame: CGRectMake(width/12, height/4, 10*width/12, height/2))
+        button.backgroundColor = UIColor.init(red: 1, green: 0, blue: 0, alpha: 0.5)
+        button.setTitle("ROAST", forState: UIControlState.Normal)
+        button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        button.titleLabel?.font = UIFont.init(name: "Papyrus", size: 25)
+        button.addTarget(self, action: #selector(roast), forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    func roast(sender:UIButton){
+        let soundURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("roasted1", ofType: "mp3")!)
+        playSound(soundURL)
+    }
+    
+    func playSound(url:NSURL){
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOfURL:url)
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }catch {
+            print("Error getting the audio file")
+        }
     }
 
     override func didReceiveMemoryWarning() {
